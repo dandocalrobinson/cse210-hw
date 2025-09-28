875bce95dcfefb3c0d6ddfdc30ee73d014749d31
Daniel Robinson <dandocalrobinson@gmail.com>
2025-09-27 20:57:26 -0600

    Made List Generator

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,52 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        List<int> numbs = new List<int>();
+        Console.WriteLine("Enter list of numbers (type '0' when you are finished):");
+        while (true)
+        {
+            Console.Write("Enter a number: ");
+            string inputnum = Console.ReadLine();
+            if (int.TryParse(inputnum, out int number))
+            {
+                if (number == 0)
+                {
+                    break;
+                }
+                numbs.Add(number);
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number. ");
+            }
+
+        }
+        Console.WriteLine("You entered the following numbers:");
+        foreach (int num in numbs)
+        {
+            Console.WriteLine(num);
+        }
+        int sum = 0;
+        foreach (int num in numbs)
+        {
+            sum += num;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        double average = (double)sum / numbs.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int largest = numbs[0];
+        foreach (int num in numbs)
+        {
+            if (num > largest)
+            {
+                largest = num;
+            }
+        }
+        Console.WriteLine($"The largest number is: {largest}");
+        
     }
 }
