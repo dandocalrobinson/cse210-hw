13053cd735f80f3510373af0b7472c0d287ca8bd
Daniel Robinson <dandocalrobinson@gmail.com>
2025-09-27 21:34:44 -0600

    made functions program thing

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        DisplayWelcome();
+        string name = AskName();
+        int num = AskNum();
+        int BR;
+        Yearnum(out BR);
+        int squared = SquareNumber(num);
+        DisplayResult(name, squared, BR);
     }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome, !");
+    }
+    static string AskName()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine(); return name;
+    }
+    static int AskNum()
+    {
+        Console.Write("Enter your favorite number: ");
+        int num = int.Parse(Console.ReadLine());
+        return num;
+    }
+    static int Yearnum(out int BR)
+    {
+        Console.Write("Enter your birth year: ");
+        BR = int.Parse(Console.ReadLine());
+        return BR;
+    }
+    static int SquareNumber(int num)
+    {
+        int squared = num * num;
+        return squared;
+    }
+    static void DisplayResult(string name, int squared, int BR)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squared}.");
+
+        int currentYear = DateTime.Now.Year;
+        int age = currentYear - BR;
+        Console.WriteLine($"{name}, you will turn {age} this year.");
+
+    }
+    
 }
